4d98f1793f3a356e46f6c346f4b3668372c6d6c7
Adam Martin <adam.d.martin1986@gmail.com>
2020-08-13 21:08:48 -0400

    added login/signup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Sidebar from './Home/Navbar';
+import Auth from './Auth/Auth';
 
 function App() {
+  const [sessionToken, setSessionToken] = useState('');
+
+  useEffect(() => {
+    if (localStorage.getItem('token')){
+      setSessionToken(localStorage.getItem('token'));
+    }
+  }, [])
+
+  const updateToken = (newToken) => {
+    localStorage.setItem('token', newToken);
+    setSessionToken(newToken);
+    console.log(sessionToken);
+  }
+
   return (
     <div >
       <Sidebar/>
+      <Auth/>
     </div>
   );
 }
